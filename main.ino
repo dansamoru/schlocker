743a847d3593f9834eb281b1d785154245bea6e2
dansamoru <44676201+dansamoru@users.noreply.github.com>
2020-02-24 22:19:20 +0300

    Прикрутка элементов
    - Добавлен сканер
    - Добавлены лампочки индикации

--- main.ino
+++ main.ino
@@ -9,15 +9,12 @@
 
 //  ===INCLUDE SETTINGS===
 
-// For CLion:
+//For Arduino IDE
+#include <Wire.h>
+#include <SPI.h>
+#include <Servo.h>
+#include <Adafruit_PN532.h>
 
-#include <Arduino.h>
-#include <Servo/Servo.h>
-
-
-//For Arduino IDE:
-
-//#include <Servo.h>
 
 
 
@@ -29,6 +26,9 @@
 #define SCANNER_PIN 7  //  Pin for RFID-scanner
 #define OPEN_BUTTON_PIN 8  //  Pin for "Green" button
 #define DELETE_BUTTON_PIN 9  //  Pin for "Red" button
+#define GREEN_LED_PIN 20
+#define YELLOW_LED_PIN 20
+#define RED_LED_PIN 20
 
 //  ===HARDWARE SETTINGS===
 #define DEBUG true  //  Switch on debug (boolean)
@@ -36,7 +36,7 @@
 #define CLOSE_ANGLE 10  //  Cell closing angle (Default for first start)
 #define SCANNER_WAIT_TIME 10000  //  Total delay for scanning
 #define LOOP_DELAY 100  //  Delay in "loop"
-
+#define SERIAL_SPEED 9600
 
 
 //  ===CODE===
@@ -47,17 +47,19 @@ class Debugger {
 public:
 
     //  Free RAM on the board
-    #if DEBUG
+#if DEBUG
+
     int freeRam() {
         extern int __heap_start, *__brkval;
         int v;
         return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
     }
-    #endif
+
+#endif
 
     //  First message
     void loaded() {
-        #if DEBUG
+#if DEBUG
         if (DEBUG) {
             Serial.println("###LOADED###");
             Serial.println("#TECHNICAL INFORMATION#");
@@ -65,31 +67,49 @@ public:
             Serial.println(freeRam());
             Serial.println("###END###\n");
         }
-        #endif
+#endif
     }
 
-    //  Log acts
+    //  Acr logs
     void act(String object, String act, int number = -1, bool ending = true) {
-        #if DEBUG
-            if (lastEnds) {
-                Serial.println("#ACT#");
-                Serial.print("Object: ");
-                Serial.print(object);
-                if (number != -1) {
-                    Serial.print(" (");
-                    Serial.print(number);
-                    Serial.print(")");
-                }
-                Serial.print("\n");
+#if DEBUG
+        if (lastEnds) {
+            Serial.println("#ACT#");
+            Serial.print("Object: ");
+            Serial.print(object);
+            if (number != -1) {
+                Serial.print(" (");
+                Serial.print(number);
+                Serial.print(")");
             }
-            Serial.print("Act: ");
-            Serial.println(act);
-            lastEnds = false;
-            if (ending) {
-                lastEnds = true;
-                Serial.println("#END#\n");
-            }
-        #endif
+            Serial.print("\n");
+        }
+        Serial.print("Act: ");
+        Serial.println(act);
+        lastEnds = false;
+        if (ending) {
+            lastEnds = true;
+            Serial.println("#END#\n");
+        }
+#endif
+    }
+
+    //  Error logs
+    void error(String object, String error, int number = -1) {
+#if DEBUG
+        Serial.println("#ERROR#");
+        Serial.print("Object: ");
+        Serial.print(object);
+        if (number != -1) {
+            Serial.print(" (");
+            Serial.print(number);
+            Serial.print(")");
+        }
+        Serial.print("\n");
+        Serial.print("Error: ");
+        Serial.println(error);
+        Serial.println("#END#\n");
+#endif
     }
 };
 
@@ -107,7 +127,7 @@ protected:
 
 //  Class for ports with "INPUT" mode
 class PortIn : protected Port {
-public:
+protected:
     PortIn(short _number) : Port(_number) {
         pinMode(number, INPUT);
     }
@@ -115,15 +135,19 @@ public:
 
 //  Class for ports with "OUTPUT" mode
 class PortOut : protected Port {
-public:
+protected:
     PortOut(short _number) : Port(_number) {
         pinMode(number, OUTPUT);
+    }
+
+    void set(void value) {
+        digitalWrite(number, value);
     }
 };
 
 //  Class for ports with "INPUT_PULLUP" mode
 class PortButton : protected Port {
-public:
+protected:
     PortButton(short _number) : Port(_number) {
         pinMode(number, INPUT_PULLUP);
         digitalWrite(number, HIGH);
@@ -137,7 +161,7 @@ public:
 //  Class for ports with servos
 class PortServo : protected Port {
     Servo *servo;
-public:
+protected:
     PortServo(short _number) : Port(_number) {
 
         servo = new Servo();
@@ -156,16 +180,42 @@ public:
     }
 };
 
+// Class for ports with RFID-scanner
+class PortScanner : protected Port {
+    Adafruit_PN532 *scanner;
+    uint8_t card;
+    uint8_t uid[8];
+    uint8_t uidLength;
+protected:
+    PortScanner(short _number) : Port(_number) {
+        scanner = new Adafruit_PN532(number, 100);
+        scanner->begin();
+        if (!scanner->getFirmwareVersion()) {
+            debugger.error("Scanner", "Didn't find");
+        }
+        scanner->SAMConfig();
+    }
+
+    bool isAvailable() {
+        card = scanner->readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+        return card;
+    }
+
+    long get_uid() {
+        return *(long *) uid;
+    }
+};
+
 //  Class for user with identity key
 class User {
-    int identity;
+    long identity;
 public:
     User(int _identity) {
         identity = _identity;
     }
 
     //  Get unique id
-    int get_identity() {
+    long get_identity() {
         return identity;
     }
 };
@@ -180,6 +230,21 @@ public:
         return !read();
     }
 };
+
+//  Class for indication bulbs
+class Bulb : protected PortOut {
+public:
+    Bulb(_number) : PortOut(_number);
+
+    void turnOn() {
+        set(HIGH);
+
+    }
+
+    void turnOff() {
+        set(LOW);
+    }
+}
 
 //  Component of section
 class Cell {
@@ -236,25 +301,30 @@ public:
 
 //  Class for RFID-scanner
 class Scanner {
+    PortScanner *port;
 public:
-    static bool isAvailable() {
-        return Serial.available() > 0;
+    Scanner() {
+        port = new PortServo(SCANNER_PIN);
+    }
+
+    bool isAvailable() {
+        return port->IsAvailable();
     }
 
     //  Read current value
-    int read() {
-        return Serial.parseInt();
+    long read() {
+        return port->get_uid();
     }
 
     //  The cycle of scanning
-    int scan() {
+    long scan() {
         debugger.act("Scanner", "Start scanning", -1, false);
         long startTime = millis();  //  Variable for timer
         while (millis() - startTime <= SCANNER_WAIT_TIME) {
             if (isAvailable()) {
-                int userId = read();
+                long uid = read();
                 debugger.act("Scanner", "End scanning");
-                return userId;
+                return uid;
             }
         }
         debugger.act("Scanner", "End scanning (timeout)");
@@ -265,7 +335,51 @@ public:
 
 //  Class for all led indication
 class Indication {
+    Bulb *green;
+    Bulb *yellow;
+    Bulb *red;
+    short mode;
+public:
+    Indication(short green_number, short yellow_number, short red_number) {
+        green = new Bulb(GREEN_LED_PIN);
+        yellow = new Bulb(YELLOW_LED_PIN);
+        red = new Bulb(RED_LED_PIN);
+    }
 
+    //  Set indication mode
+    void set_mode(short value) {
+        mode = value;
+    }
+
+    void refresh() {
+        switch (mode) {
+            case 0:
+                green.turnOn();
+                yellow.turnOff();
+                red.turnOff();
+                break;
+            case 1:
+                green.turnOff();
+                yellow.turnOn();
+                red.turnOff();
+                break;
+            case 2:
+                green.turnOff();
+                yellow.turnOff();
+                red.turnOn();
+                break;
+            case 3:
+                green.turnOn();
+                yellow.turnOn();
+                red.turnOff();
+                break;
+            case 2:
+                green.turnOff();
+                yellow.turnOn();
+                red.turnOn();
+                break;
+        }
+    }
 };
 
 //  Main class for section
@@ -275,6 +389,7 @@ class Schlocker {
     Indication indication;
     Button *greenButton = new Button(OPEN_BUTTON_PIN);
     Button *redButton = new Button(DELETE_BUTTON_PIN);
+    short stat;  //  Section status: 0 - green, 1 - yellow, 2 - red, 3 - green/yellow, 4 - red/yellow
 public:
     Schlocker() {
         for (int i = 0; i < CELL_QUANTITY; ++i) {
@@ -317,13 +432,18 @@ public:
                 }
             }
         }
+        for (int i = 0; i < CELL_QUANTITY; i++) {
+
+        }
+        Indication.refresh();
     }
+
 };
 
 Schlocker *schlocker;  //  It should be with "*"
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(SERIAL_SPEED);
     schlocker = new Schlocker();
     debugger.loaded();
 }
