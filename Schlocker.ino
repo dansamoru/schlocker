a4465be86cb95fb576b8b5d8618293550eb57762
pegaster <57589882+pegaster@users.noreply.github.com>
2020-06-15 13:59:13 +0300

    Написал архитектуру и реализацию
    Не проверял на компилябельность

--- Schlocker.ino
+++ Schlocker.ino
@@ -1,482 +1,265 @@
 /*
-*
-* PROJECT SCHLOCKER
-* v.0.2.0.4
-*
-* Developer: Samoilov Daniil © 2020
-* VK: @dansamoru
+ *
+ * PROJECT SCHLOCKER!
+ *
+ * Developer: Dneprov Stepan © 2020
+ * VK: @pegaster
+ *
 */
 
 
-//  ===INCLUDE SETTINGS===
+//  ===SETTINGS===
 
+//  ==INCLUDES==
+//  =SYSTEM LIBRARIES=
+#include <EEPROM.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
+//  =USER LIBRARIES=
 
-
-//  ===PROJECT SETTINGS===
+//  ==DEFINES==
+//  =PROJECT=
 #define CELL_QUANTITY 4
-
-//  ===CIRCUIT SETTINGS===
-#define LOCKER_START_PIN 9  //  First pin for cells
-#define LOCKER_SENSOR_START_PIN 5  //  First pin for checkers
-#define SCANNER_PIN 19  //  Pin for RFID-scanner
-#define OPEN_BUTTON_PIN 14  //  Pin for "Green" button
-#define DELETE_BUTTON_PIN 15  //  Pin for "Red" button
-#define GREEN_LED_PIN 2
-#define YELLOW_LED_PIN 3
-#define RED_LED_PIN 4
-
-//  ===HARDWARE SETTINGS===
-#define DEBUG true  //  Switch on debug (boolean)
-#define SCANNER_WAIT_TIME 1000  //  Total delay for scanning
-#define LOOP_DELAY 100  //  Delay in "loop"
+#define USERID_LENGTH 4  //  default — 4, secure — 7
+//  =PINS=
+#define LOCKER_START_PIN 10
+#define SENSOR_START_PIN 10
+#define SCANNER_PIN 10
+//  =CIRCUIT=
+#define LOCKERSENSOR_MODE false  //  0 — l+s, 1 — l/s
+//  =HARDWARE=
+#define LOOP_DELAY 10
 #define SERIAL_SPEED 9600
-#define LOCKER_MAX_TIME 4000 //  Max time for locker opening in milliseconds
-#define LOCKER_SENSOR_DEFAULT_VALUE false  //  Default value when locker is closed
+#define SCANER_WAIT_TIME 1000
+#define LOCKER_WAIT_TIME 1000
+#define CELL_DONT_OPEN_TIME 604800000
+//  =STRUCT TYPE=
+struct cell_data{
+  long userId;
+  unsigned long lastOpenTime;
+}
+//  =VARIABLES=
+const unsigned short cell_data_size = sizeof(cell_data);
+Adafruit_PN532 scanner(SCANNER_PIN, 100);
+unsigned short
+    lockers[CELL_QUANTITY],
+    sensors[CELL_QUANTITY];
+const unsigned short
+    greenBtn = 10,
+    redBtn = 10,
+    greenLed = 10,
+    yellowLed = 10,
+    redLed = 10;
+uint8_t uid[USERID_LENGTH];
+uint8_t uidLength{};
+//  ===INITIALIZATION===
+void pinModes();
+bool scanerSetup();
+void cellSetup();
+void memorySetup();
+void update();
+bool isReadable();
+unsigned long scan();
+void indicate(unsigned short status);
+unsigned short update_status();
+unsigned short findCellNumber(unsigned long userId);
+void unregUser(unsigned short cell_number);
+unsigned short regUser(unsigned long userId);
+void rewriteLastOpenTime(unsigned short cell_number);
+void open(unsigned short cell_number);
 
 
-//  ===CODE===
-
-// ==SYSTEM==
-
-class System{
-public:
-    //EEPROM here
-};
-
-//  Class for debugging messages
-class Debugger {
-    bool lastEnds = true;  //  Had last flag "end"
-public:
-    //  Free RAM on the board
-#if DEBUG
-    static int freeRam() {
-        extern int __heap_start, *__brkval;
-        int v;
-        return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-    }
-#endif
-    //  First message
-    static void loaded() {
-#if DEBUG
-        if (DEBUG) {
-            Serial.println("###LOADED###");
-            Serial.println("#TECHNICAL INFORMATION#");
-            Serial.print("Free RAM: ");
-            Serial.println(freeRam());
-            Serial.println("###END###\n");
-        }
-#endif
-    }
-    //  Act logs
-    void act(String object, String act, int number = -1, bool ending = true) {
-#if DEBUG
-        if (lastEnds) {
-            Serial.println("#ACT#");
-            Serial.print("Object: ");
-            Serial.print(object);
-            if (number != -1) {
-                Serial.print(" (");
-                Serial.print(number);
-                Serial.print(")");
-            }
-            Serial.print("\n");
-        }
-        Serial.print("Act: ");
-        Serial.println(act);
-        lastEnds = false;
-        if (ending) {
-            lastEnds = true;
-            Serial.println("#END#\n");
-        }
-#endif
-    }
-
-    //  Error logs
-    static void error(String object, String error, int number = -1) {
-#if DEBUG
-        Serial.println("#ERROR#");
-        Serial.print("Object: ");
-        Serial.print(object);
-        if (number != -1) {
-            Serial.print(" (");
-            Serial.print(number);
-            Serial.print(")");
-        }
-        Serial.print("\n");
-        Serial.print("Error: ");
-        Serial.println(error);
-        Serial.println("#END#\n");
-#endif
-    }
-};
-Debugger debugger;
-
-//  ==PORTS==
-
-//  Basic class for all components
-class Port {
-protected:
-    unsigned short portNumber;
-    explicit Port(unsigned short _portNumber) : portNumber(_portNumber) {}
-};
-
-//  Class for ports with "INPUT" mode
-class InPort : protected Port {
-protected:
-    explicit InPort(unsigned short _portNumber) : Port(_portNumber) {
-        pinMode(portNumber, INPUT);
-    }
-};
-
-//  Class for ports with "OUTPUT" mode
-class OutPort : protected Port {
-protected:
-    explicit OutPort(unsigned short _number) : Port(_number) {
-        pinMode(portNumber, OUTPUT);
-    }
-
-    void set(bool value) {
-        digitalWrite(portNumber, value);
-    }
-};
-
-//  Class for ports with "INPUT_PULLUP" mode
-class PullupPort : protected Port {
-protected:
-    explicit PullupPort(unsigned short _portNumber) : Port(_portNumber) {
-        pinMode(portNumber, INPUT_PULLUP);
-        digitalWrite(portNumber, HIGH);
-    }
-
-    bool read() {
-        return digitalRead(portNumber);
-    }
-};
-
-//  Class for lockers
-class LockerPort :  protected OutPort, protected InPort{
-public:
-    explicit LockerPort(unsigned short _lockerPortNumber, unsigned short _sensorPortNumber) :
-            OutPort(_lockerPortNumber),
-            InPort(_sensorPortNumber){}
-
-    //  Open locker
-    void open(unsigned int milliseconds = 1000) {
-        if(milliseconds > LOCKER_MAX_TIME){
-            return;
-        }
-        digitalWrite(OutPort::portNumber, HIGH);
-        delay(milliseconds); // TODO: переделать задержку
-        digitalWrite(OutPort::portNumber, LOW);
-    }
-
-    //  Is locker opened
-    bool is_open() {
-        return (digitalRead(InPort::portNumber)!=LOCKER_SENSOR_DEFAULT_VALUE);  // Compare with default value
-    }
-
-
-};
-
-// Class for ports with RFID-scanner
-class PortScanner : protected Port {
-    Adafruit_PN532 scanner = Adafruit_PN532(portNumber, 100);
-    uint8_t card{};
-    uint8_t uid[8]{};
-    uint8_t uidLength{};
-protected:
-    explicit PortScanner(unsigned short _portNumber) : Port(_portNumber) {
-        scanner.begin();
-        if (!scanner.getFirmwareVersion()) {
-            debugger.error("Scanner", "Didn't find");
-        }
-        scanner.SAMConfig();
-    }
-
-    bool isReadable() {
-        card = scanner.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-        return card;
-    }
-
-    unsigned long get_uid() {
-        return *(unsigned long *) uid;
-    }
-};
-
-//  Class for user with identity key
-class User {
-    unsigned long uid;
-public:
-    unsigned long get_uid() const {
-        return uid;
-    }
-
-    void set_uid(unsigned long _uid) {
-        uid = _uid;
-    }
-};
-
-/*
-class Buzzer : protected OutPort {
-public:
-    explicit Buzzer(unsigned short _portNumber) : OutPort(_portNumber) {}
-
-    void play(unsigned short mode) {
-        debugger.act("Buzzer", "Play");
-        switch (mode) {
-            case 0:
-                tone(BUZZER_TON_1, portNumber, 500);
-                break;
-        }
-    }
-};
-Buzzer buzzer = Buzzer(BUZZER_PIN);
- */
-
-//  Class for buttons
-class Button : protected PullupPort {
-public:
-    explicit Button(unsigned short _portNumber) : PullupPort(_portNumber) {}
-
-    //  Is button pressed in this moment
-    bool isPushed() {
-        return !read();
-    }
-};
-
-//  Class for indication bulbs
-class Led : protected OutPort {
-public:
-    explicit Led(unsigned short _portNumber) : OutPort(_portNumber) {}
-
-    void turnOn() {
-        set(HIGH);
-
-    }
-
-    void turnOff() {
-        set(LOW);
-    }
-};
-
-//  Component of section
-class Cell : protected LockerPort {
-    unsigned short identity;  //  Number of cell
-    unsigned int lastOpenTime = 0;  //  Time when cell was opened last time
-    unsigned int registrationTime = 0;  //  Time when cell was registered
-    bool isOpen = false;
-    User user;
-public:
-    explicit Cell(short _portNumber) : LockerPort(_portNumber + CELL_START_PIN, _portNumber + CELL_SENSOR_START_PIN) {
-        identity = portNumber - CELL_START_PIN;
-    }
-
-    //  Get unique user id
-    unsigned long userId() {
-        return user.get_uid();
-    }
-
-    //  Open cell
-    void open() {
-        debugger.act("Cell", "Open", identity);
-        isOpen = true;
-        open();
-    }
-
-    //  Unregister recorded user
-    void unreg(bool doOpen = true) {
-        debugger.act("Cell", "Unregistration", identity, false);
-        user.set_uid(0);
-        if (doOpen) {
-            open();
-        }
-    }
-
-    //  Register new user for cell
-    void reg(unsigned long userIdentity, bool doOpen = true) {
-        debugger.act("Cell", "Registration", identity, false);
-        user.set_uid(userIdentity);
-        if (doOpen) {
-            open();
-        }
-    }
-
-    bool get_isOpen() {
-        return isOpen;
-    }
-};
-
-//  Class for RFID-scanner
-class Scanner : protected PortScanner {
-public:
-    Scanner(unsigned short _portNumber) : PortScanner(_portNumber) {}
-
-    bool isAvailable() {
-        return isReadable();
-    }
-
-    //  Read current value
-    unsigned long read() {
-        return get_uid();
-    }
-
-    //  The cycle of scanning
-    unsigned long scan() {
-        buzzer.play(0);
-        debugger.act("Scanner", "Start scanning", -1, false);
-        unsigned long startTime = millis();  //  Variable for timer
-        delay(1);
-        while (millis() - startTime <= SCANNER_WAIT_TIME) {
-            if (isAvailable()) {
-                unsigned long uid = read();
-                debugger.act("Scanner", "End scanning");
-                return uid;
-            }
-            //delay(LOOP_DELAY / 10);
-        }
-        debugger.act("Scanner", "End scanning (timeout)");
-        return 0;
-    }
-
-};
-
-//  Class for all led indication
-class Indication {
-    Led green(GREEN_LED_PIN);
-    Led yellow(YELLOW_LED_PIN);
-    Led red(RED_LED_PIN);
-    unsigned short mode = 0;
-public:
-
-    //  Set indication mode
-    void setMode(unsigned short value) {
-        mode = value;
-    }
-
-    void refresh() {
-        switch (mode) {
-            case 0:
-                green.turnOn();
-                yellow.turnOff();
-                red.turnOff();
-                break;
-            case 1:
-                green.turnOff();
-                yellow.turnOn();
-                red.turnOff();
-                break;
-            case 2:
-                green.turnOff();
-                yellow.turnOff();
-                red.turnOn();
-                break;
-            case 3:
-                green.turnOn();
-                yellow.turnOn();
-                red.turnOff();
-                break;
-            case 4:
-                green.turnOff();
-                yellow.turnOn();
-                red.turnOn();
-                break;
-        }
-    }
-};
-
-//  Main class for section
-class Schlocker {
-    Cell cells[CELL_QUANTITY];
-    Scanner scanner = Scanner(SCANNER_PIN);
-    Indication indication;
-    Button greenButton = Button(OPEN_BUTTON_PIN);
-    Button redButton = Button(DELETE_BUTTON_PIN);
-    Button closeButton = Button(CLOSE_BUTTON_PIN);
-    unsigned short stat = 0;  //  Section status: 0 - green, 1 - yellow, 2 - red, 3 - green/yellow, 4 - red/yellow
-public:
-    Schlocker() {
-        for (int i = 0; i < CELL_QUANTITY; ++i) {
-            cells[i] = Cell(i);
-        }
-        indication.refresh();
-    }
-
-    //  Search cell with userId
-    unsigned short cellSearch(unsigned long userId) {
-        for (int i = 0; i < CELL_QUANTITY; ++i) {
-            if (cells[i]->userId() == userId) {
-                return i + 1;
-            }
-        }
-        return 0;
-    }
-
-    void updateStat() {
-        bool isGreen = false;
-        for (int i = 0; i < CELL_QUANTITY; i++) {
-            if (cells[i]->userId() == 0) {
-                isGreen = true;
-            }
-        }
-        if (isGreen) {
-            stat = 0;
-        } else {
-            stat = 2;
-        }
-        indication.set_mode(stat);
-    }
-
-    //  Refresh system status
-    void refresh() {
-        updateStat();
-        if (greenButton.isPushed()) {
-            debugger.act("Green button", "Pressed");
-            unsigned long userId = scanner.scan();
-            if (userId != 0) {
-                unsigned short cellId = cellSearch(userId);
-                if (cellId != 0) {
-                    cells[cellId - 1]->open();
-                } else {
-                    cells[cellSearch(0) - 1]->reg(userId);
-
-                }
-            }
-        }
-        redButton.isPushed();
-        if (redButton.isPushed()) {
-            debugger.act("Red button", "Pressed");
-            unsigned long userId = scanner.scan();
-            if (userId != 0) {
-                unsigned short cellId = cellSearch(userId);
-                if (cellId != 0) {
-                    cells[cellId - 1]->unreg();
-                }
-            }
-        }
-        if (closeButton.isPushed()) {
-            debugger.act("Close button", "Pressed");
-            for (int i = 0; i < CELL_QUANTITY; i++) {
-                if (cells[i]->get_isOpen()) {
-                    cells[i]->close();
-                }
-            }
-        }
-        indication.refresh();
-    }
-
-};
-
-Schlocker schlocker;
-
-void setup() {
+//  ===USE===
+void setup(){
     Serial.begin(SERIAL_SPEED);
-    debugger.loaded();
+    pinModes();
+    scanerSetup();
+    cellSetup();
+    memorySetup();
+}
+void loop(){
+    update();
+    delay(LOOP_DELAY);
 }
 
-void loop() {
-    schlocker.refresh();
-    delay(LOOP_DELAY);
+void update(){
+    unsigned short status = update_status();
+    unsigned short cell_number;
+    indicate(status);
+    unsigned long userId;
+    if(digitalRead(greenBtn) == LOW && digitalRead(redBtn) == HIGH){
+        userId = scan();
+        if(userId != NULL){
+            cell_number = findCellNumber(userId);
+            if(cell_number == NULL && status < 2){
+                cell_number = regUser(userId);
+            }
+            if(cell_number != NULL){
+                open(cell_number);
+                rewriteLastOpenTime(cell_number);
+            }
+        }
+    }
+    else if(digitalRead(redBtn) == LOW && digitalRead(greenBtn) == HIGH){
+        userId = scan();
+        cell_number = findCellNumber(userId);
+        if(cell_number != NULL){
+            unregUser(cell_number);
+            open(cell_number);
+        }
+    }
+}
+//  ===REALIZATION===
+void cellSetup(){
+#if LOCKERSENSOR_MODE == 0
+    for (int i = 0; i < CELL_QUANTITY; i++) {
+        lockers[i] = LOCKER_START_PIN + i;
+        pinMode(lockers[i], OUTPUT);
+        sensors[i] = SENSOR_START_PIN + i;
+        pinMode(sensors[i], INPUT);
+    }
+#elif LOCKERSENSOR_MODE == 1
+    for (int i = 0; i < CELL_QUANTITY; i++){
+        lockers[i] = LOCKER_START_PIN + (i*2);
+        pinMode(lockers[i], OUTPUT);
+        sensors[i] = SENSOR_START_PIN + (i*2);
+        pinMode(sensors[i], INPUT);
+    }
+#endif
+}
+
+void pinsMode(){
+    pinMode(greenBtn, INPUT_PULLUP);
+    pinMode(redBtn, INPUT_PULLUP);
+    pinMode(greenLed, OUTPUT);
+    pinMode(yellowLed, OUTPUT);
+    pinMode(redLed, OUTPUT);
+}
+
+void scanerSetup(){
+    scaner.begin();
+    if (!scanner.getFirmwareVersion()) {
+        Serial.println("Scaner didn't found")
+    }
+    scanner.SAMConfig();
+}
+
+void memorySetup(){
+    if(EEPROM.read(0) != CELL_QUANTITY){
+        EEPROM.write(0, CELL_QUANTITY);
+        for(unsigned short i = 1; i < CELL_QUANTITY * cell_data_size + 1; i++){
+            EEPROM.update(i, 255);
+        }
+    }
+}
+
+void isReadable(){
+    return scanner.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+}
+
+unsigned long scan(){
+    unsigned long startTime = millis();  //  Variable for timer
+    delay(1);
+    while (millis() - startTime <= SCANNER_WAIT_TIME) {
+        if (isReadable()) {
+            return *(unsigned long *) uid;
+        }
+    return NULL;
+}
+
+unsigned short findCellNumber(unsigned long userId){
+    cell_data buffer;
+    for(unsigned short i = 0; i < CELL_QUANTITY; i++){
+       EEPROM.get(i * cell_data_size + 1, buffer);
+       if(buffer.userId == userId){
+          return i;
+       }
+    }
+    return NULL;
+}
+
+unsigned short regUser(unsigned long userId){
+    cell_data buffer;
+    for(unsigned short i = 0; i < CELL_QUANTITY; i++){
+       EEPROM.get(i * cell_data_size + 1, buffer);
+       if(buffer.userId == 4294967295){
+          buffer.userId = userId;
+          EEPROM.put(i * cell_data_size + 1, buffer);
+          return i;
+       }
+    }
+}
+
+void unregUser(unsigned short cell_number){
+    EEPROM.get(cell_number * cell_data_size + 1, buffer);
+    buffer.userId = 4294967295;
+    EEPROM.put(cell_number * cell_data_size + 1, buffer));
+}
+
+void rewriteLastOpenTime(unsigned short cell_number){
+    EEPROM.get(cell_number * cell_data_size + 1, buffer);
+    buffer.lastOpenTime = millis();
+    EEPROM.put(cell_number * cell_data_size + 1, buffer));
+}
+
+void indicate(unsigned short satus){
+    switch(status){
+        case 0:
+            digitalWrite(greenLed, HIGH);
+            digitalWrite(redLed, LOW);
+            digitalWrite(yellowLed, LOW);
+            break;
+        case 1:
+            digitalWrite(redLed, LOW);
+            digitalWrite(greenLed, HIGH);
+            digitalWrite(yellowLed, HIGH);
+            break;
+        case 2:
+            digitalWrite(yellowLed, LOW);
+            digitalWrite(greenLed, LOW);
+            digitalWrite(redLed, HIGH);
+            break;
+        case 3:
+            digitalWrite(yellowLed, LOW);
+            digitalWrite(greenLed, LOW);
+            digitalWrite(redLed, HIGH);
+            break;
+    }
+}
+
+unsigned short update_status(){
+    bool isGreen = false;
+    bool isYellow = false;
+    cell_data buffer;
+    for(unsigned short i = 0; i < CELL_QUANTITY; i++){
+        EEPROM.get(i * cell_data_size + 1, buffer));
+        if(millis() - buffer.lastOpenTime > CELL_DONT_OPEN_TIME){
+            isYellow = true;
+        }
+        if(buffer.userId == 4294967295){
+            isGreen = true;
+        }
+        if(isGreen && isYellow){
+            break;
+        }
+    }
+    if(isGreen && isYellow){
+        return 1;
+    }
+    else if(isGreen && !isYellow){
+        return 0;
+    }
+    else if(!isGreen && isYellow){
+        return 2;
+    }
+    else if(!isGreen && !isYellow){
+        return 3;
+    }
+}
+
+void open(unsigned short cell_number){
+    digitalWrite(lockers[cell_number], HIGH);
+    delay(LOCKER_WAIT_TIME);
+    digitalWrite(lockers[cell_number], LOW);
 }
