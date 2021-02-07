fdea297742e63243553d77fcf63d8f512e7ae615
Samoilov Daniil <dansamoru@ya.ru>
2021-02-07 17:49:01 +0300

    Add project settings

--- lib/Settings/Settings.h
+++ lib/Settings/Settings.h
@@ -5,10 +5,37 @@
 #ifndef SCHLOCKER_SETTINGS_H
 #define SCHLOCKER_SETTINGS_H
 
+/*
+ * Set up a level of debugging
+ *
+ * Debug levels:
+ *     0 - Debug off
+ *     1 - Print main events [main]
+ *     2 - Print everything [detail]
+ *
+ */
+#define DEBUG 2
 
-class Settings {
+//  PROJECT CONFIGURATIONS
+#define LOCKERS_QUANTITY 4U
 
-};
+// Pins
+#define PIN_LOCKER_START 1U
+#define PIN_SENSOR_START 6U
 
+
+// === END OF SETTINGS===
+
+// System debug configuration
+#if DEBUG == 2
+#define DEBUG_detail(val) Serial.print(val);
+#define DEBUG_main(val) Serial.print(val);
+#elif DEBUG == 1
+#define DEBUG_detail(val)
+#define DEBUG_main(val) Serial.print(val);
+#else
+#define DEBUG_detail(val)
+#define DEBUG_main(val)
+#endif
 
 #endif //SCHLOCKER_SETTINGS_H
