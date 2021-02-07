789aef0c784b98dc079f0991f1a3dee361ac3042
Samoilov Daniil <dansamoru@ya.ru>
2021-02-07 14:21:52 +0300

    Add Constants

---
+++ lib/Constants/Constants.h
@@ -0,0 +1,13 @@
+//
+// Created by dansamoru on 07.02.2021.
+//
+
+#ifndef SCHLOCKER_CONSTANTS_H
+#define SCHLOCKER_CONSTANTS_H
+
+
+static const short LOCKERS_QUANTITY = 4;
+static const short CARD_NUMBER_LENGTH = 8;
+
+
+#endif //SCHLOCKER_CONSTANTS_H
