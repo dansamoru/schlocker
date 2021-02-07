f7b41adfd74c4edfed48cce8fb7836cb51d65ce6
Samoilov Daniil <dansamoru@ya.ru>
2021-02-07 17:50:16 +0300

    Add start of program

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
 #include <Arduino.h>
+
+#include <Settings.h>
+#include <Constants.h>
+#include <Models.h>
+
+Cell cells[LOCKERS_QUANTITY];
+
+void cellsSetup() {
+    DEBUG_main("#Pin setup\n");
+    // Set up lockers
+    DEBUG_detail("Set up lockers:\n");
+    for (short i = 0; i < LOCKERS_QUANTITY; ++i) {
+        cells[i].id = i;
+        cells[i].locker_pin = PIN_LOCKER_START + i;
+        cells[i].sensor_pin = PIN_SENSOR_START + i;
+
+        pinMode(cells[i].locker_pin, OUTPUT);
+        pinMode(cells[i].sensor_pin, INPUT);
+
+
+        DEBUG_detail("   locker[");
+        DEBUG_detail(i);
+        DEBUG_detail("]: id=");
+        DEBUG_detail(cells[i].id);
+        DEBUG_detail(" locker_pin=");
+        DEBUG_detail(cells[i].locker_pin);
+        DEBUG_detail(" sensor_pin=");
+        DEBUG_detail(cells[i].sensor_pin);
+        DEBUG_detail("\n");
+    }
+}
+
 void setup() {
-// write your initialization code here
+    // Turn on Serial for debugging
+#if DEBUG != 0
+    Serial.begin(9600);
+#endif
+    DEBUG_main("#Serial connected\n")
+
+    cellsSetup();
 }
 
 void loop() {
-// write your code here
+    delay(DELAY_TIME);
 }
