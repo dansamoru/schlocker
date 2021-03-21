7be1b3d2a1cace9426b5658761071b576ad748a0
Samoilov Daniil <dansamoru@ya.ru>
2021-03-21 14:12:41 +0300

    Update pins' numbers

--- lib/Settings/Settings.h
+++ lib/Settings/Settings.h
@@ -22,26 +22,26 @@
  *
  * Input levels:
  *     0 - Input nothing
- *     1 - Input user
+ *     1 - Input user only
  *     2 - Input everything
  */
-#define INPUT_DEBUG 2
+#define INPUT_DEBUG 0
 
 //  =PROJECT CONFIGURATIONS=
 #define LOCKERS_QUANTITY 4
-#define CARD_NUMBER_LENGTH 2
+#define CARD_NUMBER_LENGTH 4
 #define DELAY_TIME 100
 #define LOCKER_DELAY 1000
 #define INPUT_DELAY 10
 
 // Pins
-#define PIN_LOCKER_START 0
-#define PIN_SENSOR_START 0
+#define PIN_LOCKER_START 9
+#define PIN_SENSOR_START 3
 
-#define PIN_SCANNER 0
+#define PIN_SCANNER 17
 
-#define PIN_BUTTON_GREEN 0
-#define PIN_BUTTON_RED 0
+#define PIN_BUTTON_GREEN 15
+#define PIN_BUTTON_RED 16
 
 
 // === END OF SETTINGS===
